aa4ae1ca100b4f68f554b01631ca3555df34563e
olegdayo <ol-sidorenkov@mail.ru>
2025-01-12 22:35:33 +0300

    execute shell

--- containers/container.c
+++ containers/container.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int change_directory() {
+    printf("Changing the root directory\n");
 }
 
 int set_resourse_limits() {
+    printf("Applying a c_group\n");
 }
 
 int set_namespace() {
+    printf("Creating a namespace\n");
+}
+
+int run(char** argv) {
+    printf("Starting the container\n");
+    system("sh");
 }
 
 int main(int argc, char** argv) {
+    run(argv);
     return 0;
 }
