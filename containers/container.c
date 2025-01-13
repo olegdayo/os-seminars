05bb8b16bf37e0578fc52f7f2a79a29c68d231b2
olegdayo <ol-sidorenkov@mail.ru>
2025-01-14 01:12:27 +0300

    chdir

--- containers/container.c
+++ containers/container.c
@@ -2,27 +2,43 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+// Hide node's filesystem.
 int change_directory() {
     printf("Changing the root directory\n");
+    mkdir("/tmp/container", 0777);
+    chroot("/tmp/container-fs"); // TODO: WTF???
+    chdir("/");
 }
 
+// Hide node's processes.
 int set_namespace() {
     printf("Creating a namespace\n");
 }
 
+// Limit resources aquired from the node.
 int set_resourse_limits() {
     printf("Applying a c_group\n");
 }
 
+// Change host
+int update_networking() {
+    printf("Setting up networking");
+}
+
+// Execute the commands.
 int run(char** argv) {
     printf("Starting the container\n");
     execv(argv[0], argv);
+    printf("Killing the container\n");
 }
 
 int main(int argc, char** argv) {
     change_directory();
     set_namespace();
+    update_networking();
     set_resourse_limits();
     if (!strcmp(argv[1], "run")) {
         run(argv + 2);
