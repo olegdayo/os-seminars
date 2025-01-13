1d63a954d002a21dc7931401576f9c0e3dfd0801
olegdayo <ol-sidorenkov@mail.ru>
2025-01-13 12:47:52 +0300

    execute shell

--- containers/container.c
+++ containers/container.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int change_directory() {
     printf("Changing the root directory\n");
@@ -16,10 +17,12 @@ int set_namespace() {
 
 int run(char** argv) {
     printf("Starting the container\n");
-    system("sh");
+    execv(argv[0], argv);
 }
 
 int main(int argc, char** argv) {
-    run(argv);
+    if (!strcmp(argv[1], "run")) {
+        run(argv + 2);
+    }
     return 0;
 }
