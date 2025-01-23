152142bbfb4fed8e5375fe99c98972e60ba9d663
olegdayo <ol-sidorenkov@mail.ru>
2025-01-23 19:28:25 +0300

    some go code

--- containers/go/container.go
+++ containers/go/container.go
@@ -1,6 +1,28 @@
 package main
 
-func changeDirectory() error {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func changeDirectory(path string) error {
+	err := syscall.Mkdir(path, 0777)
+	if err != nil {
+		return err
+	}
+
+	err = syscall.Chroot(path)
+	if err != nil {
+		return err
+	}
+
+	// err = os.Chdir(path)
+	// if err != nil {
+	// 	return err
+	// }
+
 	return nil
 }
 
@@ -16,9 +38,32 @@ func updateNetwork() error {
 	return nil
 }
 
-func run() error {
+func run(argv []string) error {
+	cmd := exec.Command(argv[0], argv[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	argv := os.Args
+	fmt.Println(argv)
+	switch argv[1] {
+	case "run":
+		checkErr(changeDirectory("/tmp/container"))
+		checkErr(run(argv[2:]))
+	}
 }
